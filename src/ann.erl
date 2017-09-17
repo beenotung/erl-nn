a534b3dc35ff7bf53be52ba3348f35156cf370ac
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-09-17 21:08:41 +0800

    adding sensitivities

--- src/ann.erl
+++ src/ann.erl
@@ -1,58 +1,87 @@
 -module(ann).
 -export([
-  perceptron/3, sigmoid/1, dot_prod/2, feed_forward/2,
-  replace_input/2, convert_to_list/1,
-  connect/2
+  perceptron/3, sigmoid/1, dot_prod/2,
+  replace_input/2,
+  connect/2,
+  add/2, mul/2, foldlZipWith/5
 ]).
+
+-record(input, {pid, weight}).
+
+-record(output, {pid, sensitivity}).
+
+%% math
 
 sigmoid(X) ->
   1 / (1 + math:exp(-X)).
 
-dot_prod(X, Y) -> dot_prod(X, Y, 0).
+sigmoid_deriv(X) ->
+  math:exp(-X) / (1 + math:exp(-2 * X)).
 
-dot_prod([], [], Acc) -> Acc;
-dot_prod([X | Xs], [Y | Ys], Acc) ->
-  dot_prod(Xs, Ys, Acc + X * Y).
+add(X, Y) -> X + Y.
 
-feed_forward(Ws, Xs) ->
-  sigmoid(dot_prod(Ws, Xs)).
+mul(X, Y) -> X * Y.
 
-perceptron(Ws, Inputs, Output_PIDs) ->
+%%dot_prod(Xs, Ys) -> foldZipWith(fun add/2, fun mul/2, Xs, Ys, 0).
+
+dot_prod(Xs, Ys) -> zipWithAcc(
+  fun(Acc, X, Y) -> Acc + X * Y end,
+  Xs,
+  Ys,
+  0
+).
+
+%% core logic
+
+feed_forward(F, Ws, Xs) ->
+  F(dot_prod(Ws, Xs)).
+
+-spec perceptron(Weights, Inputs, Outputs) -> term() when
+  Weights :: [integer()],
+  Inputs :: [#input{}],
+  Outputs :: [#output{}].
+
+perceptron(Ws, Inputs, Outputs) ->
+  Self = self(),
   receive
     {stimulate, Input} ->
       New_Inputs = replace_input(Inputs, Input),
-      Y = feed_forward(Ws, convert_to_list(New_Inputs)),
-      if Output_PIDs =/= [] ->
+      Y = feed_forward(fun sigmoid/1, Ws, convert_to_input_weights(New_Inputs)),
+      if Outputs =/= [] ->
+        Msg = {Self, Y},
         lists:foreach(
-          fun(Output_PID) ->
-            Output_PID ! {stimulate, {self(), Y}}
+          fun(Output) ->
+            Output#output.pid ! {stimulate, Msg}
           end,
-          Output_PIDs
+          Outputs
         );
-        Output_PIDs =:= [] ->
-          io:format("~p outputs: ~p~n", [self(), Y])
+        Outputs =:= [] ->
+          io:format("~p outputs: ~p~n", [Self, Y]),
+          Self ! {learn, {Self, 1}}
       end,
-      perceptron(Ws, New_Inputs, Output_PIDs);
+      perceptron(Ws, New_Inputs, Outputs);
     {connect_to_output, Output_PID} ->
-      New_Output_PIDs = [Output_PID | Output_PIDs],
-      io:format("~p output connected to ~p: ~p~n", [self(), Output_PID, New_Output_PIDs]),
-      perceptron(Ws, Inputs, New_Output_PIDs);
+      New_Outputs = [#output{pid = Output_PID, sensitivity = 1} | Outputs],
+      io:format("~p output connected to ~p: ~p~n", [Self, Output_PID, New_Outputs]),
+      perceptron(Ws, Inputs, New_Outputs);
     {connect_to_input, Input_PID} ->
       W = 0.5,
       New_Inputs = [{Input_PID, W} | Inputs],
-      io:format("~p inputs connected to ~p: ~p~n", [self(), Input_PID, New_Inputs]),
-      perceptron([W | Ws], New_Inputs, Output_PIDs);
+      io:format("~p inputs connected to ~p: ~p~n", [Self, Input_PID, New_Inputs]),
+      perceptron([W | Ws], New_Inputs, Outputs);
     {pass, X} ->
-      Output = {self(), X},
+      Msg = {Self, X},
       lists:foreach(
-        fun(Output_PID) ->
-          io:format("Stimulating ~p with ~p~n", [Output_PID, X]),
-          Output_PID ! {stimulate, Output}
+        fun(Output) ->
+          io:format("Stimulating ~p with ~p~n", [Output#output.pid, X]),
+          Output#output.pid ! {stimulate, Msg}
         end,
-        Output_PIDs
+        Outputs
       ),
-      perceptron(Ws, Inputs, Output_PIDs)
+      perceptron(Ws, Inputs, Outputs)
   end.
+
+%% helper functions
 
 connect(Input_PID, Output_PID) ->
   Input_PID ! {connect_to_output, Output_PID},
@@ -62,11 +91,15 @@ replace_input(Xs, Input) ->
   {Input_PID, _} = Input,
   lists:keyreplace(Input_PID, 1, Xs, Input).
 
-convert_to_list(Tups) ->
-  lists:map(
-    fun(Tup) ->
-      {_, X} = Tup,
-      X
-    end,
-    Tups).
+convert_to_input_weights(Inputs) -> lists:map(fun(Input) ->
+  Input#input.weight end, Inputs).
 
+%% override std impl
+
+zipWithAcc(_, [], [], Acc) -> Acc;
+zipWithAcc(F, [X | Xs], [Y | Ys], Acc) ->
+  zipWithAcc(F, Xs, Ys, F(Acc, X, Y)).
+
+%% utils
+foldlZipWith(F_Acc, F_Combine, Xs, Ys, Acc) ->
+  lists:foldl(F_Acc, Acc, lists:zipwith(F_Combine, Xs, Ys)).
